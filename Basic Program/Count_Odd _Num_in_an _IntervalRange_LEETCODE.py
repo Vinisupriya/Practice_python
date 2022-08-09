f3ff2100809455b3bfd83b832d3b2b48e3316e6d
Coolshot <vnisuprya@gmail.com>
2022-08-10 00:09:50 +0530

    Added basic program

---
+++ Basic Program/Count_Odd _Num_in_an _IntervalRange_LEETCODE.py
@@ -0,0 +1,17 @@
+#Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+#Example 1:
+#Input: low = 3, high = 7
+#Output: 3
+#Explanation: The odd numbers between 3 and 7 are [3,5,7].
+#CODE
+low =1
+high=10
+k=[]
+for i in range(low,high+1):
+    if i%2 == 0:
+        continue
+    else:
+        k= +i
+        print( "The odd numbers between" + str(low) + "and "+ str(high)+" are", k)
+
+

cc9b77ea6758f4920acef305f2801758173340d9
Coolshot <vnisuprya@gmail.com>
2022-08-14 22:43:26 +0530

    update File_handling.py

--- Basic Program/File_handling.py
+++ Basic Program/File_handling.py
@@ -1,2 +1,20 @@
-file = open("vini.txt" ,"r" )
-print(file.read())
+# Read a file
+
+'''file = open("vini.txt" ,"r" )
+print(file.read())'''
+
+# Write a file
+
+try:
+    file = open("vini.txt" ,"w" )
+file.write("Hello world")
+file.close()
+
+file= open("vini.txt","r")
+for line in file:
+    print(line)
+
+except FileNotFoundError:
+    print("Error")
+else:
+    file.close()

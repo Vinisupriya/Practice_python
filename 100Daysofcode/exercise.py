6bbcd162b9e227eb9db52f000d910d2e41c258fc
Coolshot <vnisuprya@gmail.com>
2022-09-16 22:04:05 +0530

    update exercise.py

--- 100Daysofcode/exercise.py
+++ 100Daysofcode/exercise.py
@@ -7,6 +7,7 @@ bmi = (wt) / (ht**2)
 print(int(bmi))
 
 # 2 Adds the digits in a 2 digit number. 
+#new problem
 # e.g. if the input was 35, then the output should be 3 + 5 = 8
 
 two_digit_number = input("Type a two digit number: ")

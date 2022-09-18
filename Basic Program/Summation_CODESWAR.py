c827a5e8700ee1bb53c3d9a4525851e4c0c8c764
Coolshot <vnisuprya@gmail.com>
2022-09-18 21:39:20 +0530

    update Summation_CODESWAR.py

--- Basic Program/Summation_CODESWAR.py
+++ Basic Program/Summation_CODESWAR.py
@@ -2,6 +2,7 @@
 # The number will always be a positive integer greater than 0.
 #For example:summation(2) -> 3 1 + 2 summation(8) -> 36 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
 #FIB
+#data
 
 def summation(num):
     sum = 0

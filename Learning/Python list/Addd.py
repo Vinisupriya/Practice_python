66e49c5c477beb2e17c20f75e0d7b8664aab3f44
Coolshot <vnisuprya@gmail.com>
2022-09-23 19:09:39 +0530

    feat: create Addd.py

---
+++ Learning/Python list/Addd.py
@@ -0,0 +1,14 @@
+Addition of elements in a List
+   
+# Creating a List
+List = [1,2,3,4]
+print("Initial List: ")
+print(List)
+  
+# Addition of Element at 
+# specific Position
+# (using Insert Method)
+List.insert(3, 12)
+List.insert(0, 'Geeks')
+print("\nList after performing Insert Operation: ")
+print(List)

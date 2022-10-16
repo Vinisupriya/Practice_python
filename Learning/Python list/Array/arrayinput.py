6d47dc00ad7f0f85002d83b085ab3ec6bcd63b59
Coolshot <vnisuprya@gmail.com>
2022-10-16 20:41:16 +0530

    feat: create arrayinput.py

---
+++ Learning/Python list/Array/arrayinput.py
@@ -0,0 +1,8 @@
+# How to get input from user in Array
+
+n= int(input("Enter your length of the array"))
+arr = []
+for i in range( n-1):
+    m = int(input("Enter the element"))
+    arr.append(m)
+print(arr)

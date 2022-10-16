86255cf39683fbfb4d25ddec35cacd16f52f5d69
Coolshot <vnisuprya@gmail.com>
2022-10-16 20:59:34 +0530

    update arrayinput.py

--- Learning/Python list/Array/arrayinput.py
+++ Learning/Python list/Array/arrayinput.py
@@ -6,3 +6,20 @@ for i in range( n-1):
     m = int(input("Enter the element"))
     arr.append(m)
 print(arr)
+
+# Add elements in array
+
+sum=0
+for i in arr:
+    sum = sum + i
+print(sum)
+
+# summation of natural number
+total = int( n * (n+1)/2)
+print(total)
+#To find missing number in array
+result = total - sum
+print(f"The missing value is {result}")
+
+
+

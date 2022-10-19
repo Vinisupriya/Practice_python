df553ae7010fb226b110a908fccb7135090746b0
Coolshot <vnisuprya@gmail.com>
2022-10-19 21:41:50 +0530

    update duplicate_values.py

--- Leetcode/Array and Hash/duplicate_values.py
+++ Leetcode/Array and Hash/duplicate_values.py
@@ -1,6 +1,6 @@
-// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+'''Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
-'''Example 1:
+Example 1:
 
 // Input: nums = [1,2,3,1]
 // Output: true
@@ -30,21 +30,21 @@
 // nums = [4,5,6,7,2,4] BCR = O(N)
 // Map = O(1) time complexity, if any element is there
 
-// map = { 4: "true', 5: true, 6: true, 7:true, 2:true,}
-// map = { 1: true, 8: true, ...100:true}
+ map = { 4: "true', 5: true, 6: true, 7:true, 2:true,}
+ map = { 1: true, 8: true, ...100:true}
 
 '''
 
-  def containsDuplicate(nums):
+def containsDuplicate(nums):
     map = {}
 
     for n in nums:
       if n not in map:
-        map[n] = true;
+        map[n] = True
       else:
-        return true
+        return True
   
-    return false
+    return False
 
 
     nums= [1,2,3,4,]
@@ -60,5 +60,5 @@ def containsDuplicate(nums):
             return True
     return False
 
-result = containsDuplicate(nums)
+result = containsDuplicate()
 print(result)
